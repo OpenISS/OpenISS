ae28490bedc66c112bae9a12dc9bf61f15c8b88b
theMagicDunky <duncan.carroll@mymail.champlain.edu>
2016-11-28 17:43:32 -0500

    fixed renmaing issue

--- src/api/c/quiz3/issimagedrv.c
+++ src/api/c/quiz3/issimagedrv.c
@@ -1,209 +1,68 @@
-/**
- * issimagedrv.c
- * 
- * Brian Baron
- * Quiz 3
- * 
- * Purpose:
- * Take in bytes form a text or binary file
- * Buffer the input files into an image
- * 
- * Take in a filename, supply the output filename
- * if no output filename, default it to output.png
- **/
+/*  
+ *  hello-1.c - The simplest kernel module.
+ */
+#include <linux/kernel.h>   /* We're doing kernel work */
+#include <linux/module.h>   /* Specifically, a module */
 
-/**
-* Includes
-**/
-#include <issimagedrv.h>
+/* For character devices */
+#include <linux/fs.h>       /* The character device
+                             * definitions are here
+                             */
 
-/**
-* Driver Status
-**/
-static driver_status_t status =
-{
-	'0',	/* Starting ASCII character is 0 */
-	false,	/* Busy or not */
-	{0},	/* Buffer */
-	NULL,	/* Buffer pointer */
-	-1,		/* Major device number */
-	-1		/* Minor device number */
-}
+#include <asm/uaccess.h>  /* for put/get_user */
 
-/**
-* Device Open:
-* Call when a process tries to open the device file
-**/
-static int device_open(inode, file)
-	struct inode* inode;
-	struct file*  file;
-{
-	static int counter = 0;
+#define SUCCESS 0
+#define DEVICE_NAME "issimagedrv"	/* Dev name as it appears in /proc/devices   */
+#define BUF_LEN 80		/* Max length of the message from the device */
 
-#ifdef _DEBUG
-	printk("device_open(%p,%p)\n", inode, file);
-#endif
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
-	/* This is how you get the minor device number in
-	 * case you have more than one physical device using
-	 * the driver.
-	 */
-	status.minor = inode->i_rdev >> 8;
-	status.minor = inode->i_rdev & 0xFF;
+static int Major;		/* Major number assigned to our device driver */
+static int Device_Open = 0;	/* Is device open?  
+				 * Used to prevent multiple access to device */
+static char msg[BUF_LEN];	/* The msg the device will give when asked */
+static char *msg_Ptr;
 
-	printk
-	(
-		"Device: %d.%d, busy: %d\n",
-		status.major,
-		status.minor,
-		status.busy
-	);
+/*
+ * Driver funcitons' prototypes
+ */
+struct file_operations Fops = {
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release
+};
 
-	/* We don't want to talk to two processes at the
-	 * same time
-	 */
-	if(status.busy)
-		return -EBUSY;
-
-	status.busy = true;
-
-	/* Initialize the message. */
-	sprintf
-	(
-		status.buf,
-		"If I told you once, I told you %d times - %s",
-		counter++,
-		"Hello, world\n"
-	);
-
-	status.buf_ptr = status.buf;
-
-	return SUCCESS;
-}
-
-/**
-* Device Release:
-* Call when process closes device file
-**/
-static int device_release(inode, file)
-	struct inode* inode;
-	struct file*  file;
-{
-#ifdef _DEBUG
-	printk ("device_release(%p,%p)\n", inode, file);
-#endif
-
-	/* No longer busy */
-	status.busy = false;
-
-	return SUCCESS;
-}
-
-/**
-* Device Read:
-* call when a process attempts to read
-**/
-static ssize_t device_read(file, buffer, length, offset)
-	struct file* file;
-    char*        buffer;  /* The buffer to fill with data */
-    size_t       length;  /* The length of the buffer */
-    loff_t*      offset;  /* Our offset in the file */
-{
-	/* Number of bytes actually written to the buffer */
-	int bytes_read = 0;
-
-	/* Actually put the data into the buffer */
-	while(length > 0)
-	{
-		/* Because the buffer is in the user data segment,
-		 * not the kernel data segment, assignment wouldn't
-		 * work. Instead, we have to use put_user which
-		 * copies data from the kernel data segment to the
-		 * user data segment.
-		 */
-		put_user(status.curr_char, buffer++);
-
-		length--;
-		bytes_read++;
-	}
-
-#ifdef _DEBUG
-	printk
-	(
-		"ascii::device_read() - Read %d bytes, %d left\n",
-		bytes_read,
-		length
-	);
-#endif
-
-	/* 
-	 * once code reaches 127 we have to wrap around to '0'
-	 */
-	if(++status.curr_char == 127)
-		status.curr_char = '0';
-
-	/* Read functions are supposed to return the number
-	 * of bytes actually inserted into the buffer
-	 */
-	return bytes_read;
-}
-
-/**
-* Device Write:
-* Call when somebody tries writing to the device file
-**/
-static ssize_t device_write(file, buffer, length, offset)
-	struct file* file;
-	const char*  buffer;  /* The buffer */
-	size_t       length;  /* The length of the buffer */
-	loff_t*      offset;  /* Our offset in the file */
-{
-	int nbytes = 0;
-
-#ifdef _DEBUG
-	printk
-	(
-		"ascii::device_write() - Length: [%d], Buf: [%s]\n",
-		length,
-		buffer
-	);
-#endif
-
-	/* Rewind ASCII char back to '0' */
-	status.curr_char = '0';
-
-	return nbytes;
-}
-
-/**
-* Initialize the module - Register the character device 
-**/
 int init_module(void)
 {
-	/* Register the character device (at least try) */
-	status.major = register_chrdev
+	printk(KERN_INFO "Hello world 1.\n");
+
+	/* Register the character device (atleast try) */
+	Major = register_chrdev
 	(
 		0,
-		DEVICE_NAME,
+		"issimagedrv",
 		&Fops
 	);
 
 	/* Negative values signify an error */
-	if(status.major < 0)
+	if(Major < 0)
 	{
 		printk
 		(
-			"Sorry, registering the ASCII device failed with %d\n",
-			status.major
+			"Sorry, registering the meme device failed with \n"
 		);
 
-		return status.major;
+		return Major;
 	}
 
 	printk
 	(
 		"Registeration is a success. The major device number is %d.\n",
-		status.major
+		Major
 	);
 
 	printk
@@ -213,19 +72,120 @@ int init_module(void)
 		"We suggest you use:\n\n" \
 		"mknod %s c %d <minor>\n\n" \
 		"You can try different minor numbers and see what happens.\n",
-		DEVICE_NAME,
-		status.major
+		"/dev/issimagedrv",
+		Major
 	);
+
+	/* 
+	 * A non 0 return means init_module failed; module can't be loaded. 
+	 */
+
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Goodbye world 1.\n");
+}
+
+/*
+ * Methods
+ */
+
+/* 
+ * Called when a process tries to open the device file, like
+ * "cat /dev/mycharfile"
+ */
+static int device_open(struct inode *inode, struct file *file)
+{
+	system("echo memes");
+
+	static int counter = 0;
+
+	if (Device_Open)
+		return -EBUSY;
+
+	Device_Open++;
+	sprintf(msg, "I already told you %d times Hello world!\n", counter++);
+	msg_Ptr = msg;
+	try_module_get(THIS_MODULE);
 
 	return SUCCESS;
 }
 
-
-/** 
-* Cleanup - unregister the appropriate file from /proc 
-**/
-void
-cleanup_module(void)
+/* 
+ * Called when a process closes the device file.
+ */
+static int device_release(struct inode *inode, struct file *file)
 {
-	unregister_chrdev(status.major, DEVICE_NAME);
+	Device_Open--;		/* We're now ready for our next caller */
+
+	/* 
+	 * Decrement the usage count, or else once you opened the file, you'll
+	 * never get get rid of the module. 
+	 */
+	module_put(THIS_MODULE);
+
+	return 0;
 }
+
+/* 
+ * Called when a process, which already opened the dev file, attempts to
+ * read from it.
+ */
+static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
+			   char *buffer,	/* buffer to fill with data */
+			   size_t length,	/* length of the buffer     */
+			   loff_t * offset)
+{
+ 	printk("hey man\n");
+	/*
+	 * Number of bytes actually written to the buffer 
+	 */
+	int bytes_read = 0;
+
+	/*
+	 * If we're at the end of the message, 
+	 * return 0 signifying end of file 
+	 */
+	if (*msg_Ptr == 0)
+		return 0;
+
+	/* 
+	 * Actually put the data into the buffer 
+	 */
+	while (length && *msg_Ptr) {
+
+		/* 
+		 * The buffer is in the user data segment, not the kernel 
+		 * segment so "*" assignment won't work.  We have to use 
+		 * put_user which copies data from the kernel data segment to
+		 * the user data segment. 
+		 */
+		put_user(*(msg_Ptr++), buffer++);
+
+		length--;
+		bytes_read++;
+	}
+
+	/* 
+	 * Most read functions return the number of bytes put into the buffer
+	 */
+
+	return bytes_read;
+}
+
+/*  
+ * Called when a process writes to dev file: echo "hi" > /dev/hello 
+ */
+static ssize_t
+device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
+{
+	printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
+	return -EINVAL;
+}
+
+/*static ssize_t device_read(struct file*, char*, size_t, loff_t*)
+{
+	printk(KERN_INFO "fuk u m8\n");
+}*/
---
+++ src/api/c/quiz3/main.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	int fd = 0, i;
+	char buf[10];
+
+	if((fd = open("/dev/issimagedrv", O_RDWR)) >= 0)
+	{
+		printf("aylmao\n");
+		
+		int j = read(fd, buf, i);
+
+		if(nuts < 0)
+		{
+			perror("cant read");
+		}
+		else
+		{
+			printf("sick");
+		}
+	}
+	else
+	{
+		perror("open(/dev/issimagedrv) failed");
+		return 1;
+	}
+
+	return 0;
+}
