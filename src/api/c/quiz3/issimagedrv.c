b31eafc76ee7e3e5034120b7dd15b55e0ac567c1
root <root@localhost.localdomain>
2016-11-28 23:47:31 -0500

    open file in kernel, having problems reading it

--- src/api/c/quiz3/issimagedrv.c
+++ src/api/c/quiz3/issimagedrv.c
@@ -1,8 +1,66 @@
 /* issimagedrv.c */
 
 #include "issimagedrv.h"
+#include <linux/fs.h>
+#include <asm/segment.h>
+#include <asm/uaccess.h>
+#include <linux/buffer_head.h>
 
 static int Major; /* Major number assigned to our device driver */
+unsigned int** data[3];
+unsigned char* readData;
+unsigned int size = 255;
+struct file* thefile = NULL;
+
+
+struct file* file_open(const char* path, int flags, int rights) {
+    struct file* filp = NULL;
+    mm_segment_t oldfs;
+    int err = 0;
+
+    oldfs = get_fs();
+    set_fs(get_ds());
+    filp = filp_open(path, flags, rights);
+    set_fs(oldfs);
+    if(IS_ERR(filp)) {
+        err = PTR_ERR(filp);
+        return NULL;
+    }
+    return filp;
+}
+
+void file_close(struct file* file) {
+    filp_close(file, NULL);
+}
+
+int file_read(struct file* file, loff_t offset, unsigned char* data, unsigned int size) {
+    mm_segment_t oldfs;
+    int ret;
+
+    oldfs = get_fs();
+    set_fs(get_ds());
+
+    ret = vfs_read(file, data, size, &offset);
+    printk("\n%i\n", ret);
+
+    set_fs(oldfs);
+    return ret;
+}  
+
+int file_write(struct file* file, unsigned long long offset, unsigned char* data, unsigned int size) {
+    mm_segment_t oldfs;
+    int ret;
+
+    oldfs = get_fs();
+    set_fs(get_ds());
+
+    ret = vfs_write(file, data, size, &offset);
+
+    set_fs(oldfs);
+    return ret;
+}
+
+
 
 /* Module initially loaded */
 int init_module(void)
@@ -77,6 +135,15 @@ static ssize_t device_read(struct file *file, char *buffer, size_t length, loff_
 /* Process attempts to write to device file */
 static ssize_t device_write(struct file *file, const char *buff, size_t len, loff_t * off)
 {
-	printk("%s being written to\n", DEVICE_NAME);
+	printk("%s", buff);
+	thefile = file_open(buff, 0, 0);
+
+	int ret;
+	ret = file_read(thefile, 0, readData, size);
+
+	//printk("%i", ret);
+
+	file_close(thefile);
+	//printk("%s being written to\n", DEVICE_NAME);
 	return SUCCESS;
 }
