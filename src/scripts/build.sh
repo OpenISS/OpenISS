0c1b0369a8e62aa9230b2d218e1a59acacad59e8
student <student@localhost.localdomain>
2016-11-29 21:10:33 -0500

    Changed the build.sh shell script to handle patching and building tinyosc. Also added the needed patch file in the dependencies folder.

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -1,45 +1,34 @@
-#!/bin/bash
+#! /bin/bash
+# build.sh
+# Brian Baron, Colin Brady, Robert Gentile
+# CSI-230
+#
+# Need to be root when running this script
 
 if [[ $1 == "el6" ]]; then
-	#install dependencies
-	cd ./dependencies
-	./el6.sh
+	echo "running el6.sh"
+	./dependencies/el6.sh --install
 
-	#run cmake and make files for libfreenect
-	cd ../../../libfreenect
+	# compile the libfreenect2 stuff
+	cd ../../libfreenect2
+	rm -rf build
 	mkdir build && cd build
 	cmake -L ..
-	make
 	make install
 
-	#run install script to openframeworks
-	cd ../../openFrameworks/scripts/linux
-	#tells scripts to use 3 cpu cores compile
-
-	#cant actually compile openframeworks as 11/14/16 becuase github master branch doesn't build
-	#but it looks like theyre working on fixing
-
-	#./compileOF.sh -j3
-fi
-
-if [[ $1 == "--cleanup" ]]; then
-	#uninstall libusb
-	cd ./dependencies
-	cd libusb-1.0.20
-	make uninstall
-	cd ../
-	rm -rf libusb-1.0.20 libusb.tar.bz2
-
-	#uninstall libfreenect
-	cd ../../../libfreenect/build
-	make uninstall
-	cd ../
+	# compile opencv
+	cd ../../opencv
 	rm -rf build
+	mkdir build && cd build
+	cmake ..
+	make
 
-	#remove links created by libfreenect
-	rm -f /usr/local/lib/libfreenect*
-	rm -rf /usr/local/lib/fakenect
+	#patch and compile tinyosc
+	cd ../../tinyosc
+	patch build.sh < ../src/scripts/dependencies/tinyosc.build.sh.patch
+	./build.sh
 
-	#remove packages installed by yum
-	yum remove -y libXmu-devel libXi-devel glut-devel libudev-devel gstreamer-devel gstreamer-plugins-base-devel
+
+elif [[ $1 == "--cleanup" ]]; then
+	./dependencies/el6.sh --cleanup
 fi
