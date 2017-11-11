0b18588680d6911624a6ca341a9d3470cbf3552f
Serguei A. Mokhov <mokhov@cse.concordia.ca>
2017-11-11 13:20:52 -0500

    [CI] refactor common commands and paths; add debug info, for #5

--- tools/ci/docker_setup_tests.sh
+++ tools/ci/docker_setup_tests.sh
@@ -5,28 +5,49 @@
 # Version of CentOS/RHEL
 el_version=$1
 
+# Commands and paths
+BULDDEPS="make -f OpenISS/src/Makefile deps"
+DOCKERPATH=/OpenISS
+
+# Debug
+DEBUG=1
+
+if [ "$DEBUG" -eq "1" ]; then
+	date
+	pwd
+	ls -al
+fi
+
 # Run tests in Container
-# We use `--privileged` for cgroup compatability, which seems to be enabled by default in HTCondor 8.6.x
+# Use `--privileged` for cgroup compatability
 if [ "$el_version" -eq "6" ]; then
 
 	sudo docker run \
-		--privileged -v /sys/fs/cgroup:/sys/fs/cgroup --rm=true \
-		-v `pwd`:/OpenISS:rw centos:centos${OS_VERSION}
-		/bin/bash -c "make -f /OpenISS/src/Makefile deps"
+		--privileged \
+		-v /sys/fs/cgroup:/sys/fs/cgroup --rm=true \
+		-v `pwd`:$DOCKERPATH:rw centos:centos${OS_VERSION} \
+		/bin/bash -c $BUILDDEPS
 
 elif [ "$el_version" -eq "7" ]; then
 
 	docker run \
-		--privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup
-		-v `pwd`:/OpenISS:rw  centos:centos${OS_VERSION} /usr/sbin/init
+		--privileged -d -ti -e "container=docker"  \
+		-v /sys/fs/cgroup:/sys/fs/cgroup
+		-v `pwd`:$DOCKERPATH:rw  centos:centos${OS_VERSION} /usr/sbin/init
 
 	DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 	docker logs $DOCKER_CONTAINER_ID
 	docker exec \
-		-ti $DOCKER_CONTAINER_ID /bin/bash \
-		-xec "make -f /OpenISS/src/Makefile deps; echo -ne \"------\nEND OpenISS TESTS\n\";"
+		-ti $DOCKER_CONTAINER_ID \
+		/bin/bash -xec "$BUILDDEPS; echo -ne \"------\nEND OpenISS TESTS\n\";"
 	docker ps -a
 	docker stop $DOCKER_CONTAINER_ID
 	docker rm -v $DOCKER_CONTAINER_ID
 
 fi
+
+if [ "$DEBUG" -eq "1" ]; then
+	date
+fi
+
+# EOF
