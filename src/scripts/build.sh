6d6e2c7beb4ba0a8b6a03205152acfa9001a093f
root <root@localhost.localdomain>
2017-11-20 23:36:29 -0500

    Modified build.sh, el6.sh, and Makefile build.sh refactored and el6.sh is also refactored Makefile calls modified

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -10,17 +10,19 @@
 #
 
 tinyosc_option="--tinyosc"
-ogl_option="--ogl"
-opencv_option="--opencv"
-libfreenect_option="--libfreenect"
-libfreenect2_option="--freenect2"
 
-#alex
+do_all=1
+install_option="--install"
+cleanup_option="--cleanup"
+mode=0
+system="el6"
+el6_system="el6"
+
 function install_tinyosc()
 {
-	if [ "$(grep "tinyosc" build.cache)" != "tinyosc" ]
-	then
+	if [ "$(grep "tinyosc" build.cache)" != "tinyosc" ]; then
 		#patch and compile tinyosc
+		./dependencies/$system.sh --install --tinyosc
 		pushd ../../tinyosc
 		patch build.sh < ../src/scripts/dependencies/tinyosc.build.sh.patch
 		./build.sh
@@ -30,280 +32,35 @@ function install_tinyosc()
 		echo "tinyosc already installed"
 	fi
 }
-#alex
 function cleanup_tinyosc()
-{
-	echo "nothing to uninstall to small"
-}
-
-#Nick
-function installOpenFrameworks()
-{
-	if [ "$(grep "openframeworks" build.cache)" != "openframeworks" ]
-	then
-		#run install script to openframeworks
-		pushd ../../openFrameworks/scripts/linux
-		#tells scripts to use 3 cpu cores compile
-
-		#cant actually compile openframeworks as 11/14/16 becuase github master branch doesn't build
-		#but it looks like theyre working on fixing
-
-		#./compileOF.sh -j3
-		popd
-		echo "openframeworks" >> build.cache
+{	
+	if [ "$(grep "tinyosc" build.cache)" == "tinyosc" ]; then
+		./dependencies/$system.sh --install --tinyosc
+		sed -i '/tinyosc/d' build.cache
+		echo "tinyosc uninstalled"
 	else
-		echo "openframeworks already installed"
+		echo "tinyosc not installed"
 	fi
 }
 
-#Nick
-function installOpenFrameworksDeps()
-{
-	if [ "$(grep "openframeworks" build.cache)" != "openframeworks" ]
-	then
-		#install dependencies
-		echo "running el6.sh"
-		./dependencies/el6.sh --ofx
-		echo "openframeworks" >> build.cache
-	else
-		echo "openframeworks already installed"
-	fi
-}
-
-#Rosser
-function installOpenCV()
-{
-
-	if [ "$(grep "opencv" build.cache)" != "opencv" ]
-        then
-                # compile opencv
-                pushd ../../opencv
-                rm -rf build
-                mkdir build && cd build
-                cmake ..
-                make
-                popd
-                echo "opencv" >> build.cache
-        else
-                echo "opencv already installed"
-        fi
-		
-}
-
-function cleanupOpenCV()
-{
-	#fill this up with that sweet code namsayin
-	./el6.sh --cleanup --opencv
-}
-
-# install ogl - Matthew Roy
-function install_ogl()
-{
-	#compile ogl
-        pushd ../../ogl
-        rm -rf build
-        mkdir build && cd build
-        cmake ..
-	#make
-        popd
-        echo "ogl" >> build.cache
-}
-
-# cleanup ogl - Matthew Roy
-function cleanup_ogl()
-{
-	echo "need to actually write this function"		
-}
-
-#Cory
-function install_libfreenect()
-{
-        if [ "$(grep "libfreenect_" build.cache)" != "libfreenect_" ]
-        then
-
-                #run cmake and make files for libfreenect
-                pushd ../../libfreenect
-                mkdir build && cd build
-                # XXX: BUILD_OPENNI2_DRIVER=ON would work with cmake3 and gcc 4.8+ once installed
-                cmake \
-                        -DLIBUSB_1_LIBRARY=../../libfreenect2/depends/libusb/lib/libusb-1.0.so \
-                        -DLIBUSB_1_INCLUDE_DIR=../../libfreenect2/depends/libusb/include/libusb-1.0 \
-                        -DBUILD_OPENNI2_DRIVER=OFF \
-                        -L ..
-                make
-	        make install
-        	popd
-	        echo "libfreenect_" >> build.cache
-        else
-                echo "libfreenect already installed"
+for var in "$@"
+do
+	if [ $var == $install_option ]; then
+		mode=$install_option
+	elif [ $var == $cleanup_option ]; then
+		mode=$cleanup_option
+	elif [ $var == $el6_system ]; then
+		system=$el6_system
+	elif [ $var == $tinyosc_option ]; then
+		tinyosc_option=1
 	fi
 
-}
+done
 
-function cleanup_libfreenect()
-{
-
-        #uninstall libfreenect
-        cd ../../../libfreenect/build
-        make uninstall
-        cd ../
-        rm -rf build
-
-        #remove links created by libfreenect
-        rm -f /usr/local/lib/libfreenect*
-        rm -rf /usr/local/lib/fakenect
-}
-
-#Calum
-function install_libfreenect2()
-{
-	if [ "$(grep "libfreenect2" build.cache)" != "libfreenect2" ]
-        then
-                # compile the libfreenect2 stuff
-                pushd ../../libfreenect2
-                rm -rf build
-                mkdir build && cd build
-                cmake -L ..
-                make install
-                popd
-                echo "libfreenect2" >> build.cache
-        else
-                echo "libfreenect2 already installed"
-        fi
-}
-
-#Calum
-function cleanup_libfreenect2()
-{
-	echo "libfreenect2 uninstalled"
-}
-
-#Nick
-if [ "$1" == "--ofx" ]; then
-	installOpenFrameworks
-fi
-
-#Nick
-if [ "$1" == "--ofxdeps" ]; then
-	installOpenFrameworksDeps
-fi
-
-if [ "$1" == "el6" ]; then
-
-	if [ ! -e "build.cache" ]
-	then
-		touch build.cache
+if [ $tinyosc_option == 1 -o $do_all == 1 ]; then
+	if [ $mode == $install_option ]; then
+		install_tinyosc
+	elif [ $mode == $cleanup_option ]; then
+		cleanup_tinyosc
 	fi
-
-	if [ "$(grep "el6-dependencies" build.cache)" != "el6-dependencies" ]
-	then
-		#install dependencies
-		echo "running el6.sh"
-		./dependencies/el6.sh --install
-		echo "el6-dependencies" >> build.cache
-	else
-		echo "el6-dependencies already installed"
-	fi
-
-	if [ "$(grep "libfreenect2" build.cache)" != "libfreenect2" ]
-	then
-		# compile the libfreenect2 stuff
-		pushd ../../libfreenect2
-		rm -rf build
-		mkdir build && cd build
-		cmake -L ..
-		make install
-		popd
-		echo "libfreenect2" >> build.cache
-	else
-		echo "libfreenect2 already installed"
-	fi		
-
-	if [ "$(grep "opencv" build.cache)" != "opencv" ]
-	then
-		# compile opencv
-		pushd ../../opencv
-		rm -rf build
-		mkdir build && cd build
-		cmake ..
-		make
-		popd
-		echo "opencv" >> build.cache
-	else
-		echo "opencv already installed"
-	fi
-	
-	# alex did tinyosc
-	for var in "$@"
-	do
-		if [ $var == $tinyosc_option ]; then
-			install_tinyosc		
-                elif [ $var == $opencv_option ]; then
-                        installOpenCV
-		elif [ $var == $libfreenect_option ]; then
-                        install_libfreenect
-		elif [ $var == $libfreenect2_option ]; then
-			install_libfreenect2
-                fi
-
-	done 
-
-	if [ "$(grep "libfreenect_" build.cache)" != "libfreenect_" ]
-	then
-		#run cmake and make files for libfreenect
-		pushd ../../libfreenect
-		mkdir build && cd build
-		# XXX: BUILD_OPENNI2_DRIVER=ON would work with cmake3 and gcc 4.8+ once installed
-		cmake \
-			-DLIBUSB_1_LIBRARY=../../libfreenect2/depends/libusb/lib/libusb-1.0.so \
-			-DLIBUSB_1_INCLUDE_DIR=../../libfreenect2/depends/libusb/include/libusb-1.0 \
-			-DBUILD_OPENNI2_DRIVER=OFF \
-			-L ..
-		make
-		make install
-		popd
-		echo "libfreenect_" >> build.cache
-	else
-		echo "libfreenect already installed"
-	fi
-
-	installOpenFrameworks
-
-elif [[ "$1" == "--cleanup" ]]; then
-	./dependencies/el6.sh --cleanup
-
-	#uninstall libusb
-	cd ./dependencies
-	cd libusb-1.0.20
-	make uninstall
-	cd ../
-	rm -rf libusb-1.0.20 libusb.tar.bz2
-
-
-	for var in "$@"
-	do
-		if [ $var == $tinyosc_option ]; then
-			cleanup_tinyosc
-		elif [ $var == $opencv_option ]; then
-			cleanupOpenCV		
-		elif [ $var == $libfreenect_option ]; then
-			cleanup_libfreenect
-		elif [ $var == $libfreenect2_option ]; then
-			cleanup_libfreenect2
-		fi
-	done 
-
-	
-
-else
-	if [ "$1" == ogl_option ]; then
-		if [ "$(grep "ogl" build.cache)" != "ogl" ]
-		then
-			echo "running el6.sh ogl"
-			./dependencies/el6.sh --ogl
-		else
-			echo "ogl already installed"
-		fi
-	install_ogl
-fi
 fi
--- src/scripts/dependencies/el6.sh
+++ src/scripts/dependencies/el6.sh
@@ -13,231 +13,39 @@
 # Need to be root when running this script
 
 tinyosc_option="--tinyosc"
-opencv_option="--opencv"
-libfreenect2_option="--freenect2"
+
+install_option="--install"
+cleanup_option="--cleanup"
+mode=0
 
 function install_tinyosc()
 {
 	yum install -y gcc
 	echo "tinyosc dependencies installed"
 }
-#alex
 function cleanup_tinyosc()
 {
-	echo "nothing to uninstall to small"
-}
-
-# el6.sh - dependencies for ogl - Matthew Roy
-function install_ogl_deps()
-{
-	# dependencies for ogl
-	yum install -y cmake make gcc-c++ libX11-devel libXi-devel mesa-libGL mesa-libGLU libXrandr-devel libXext-devel libXcursor-devel libXinerama-devel libXi-devel
-}
-
-#Nick - Installs dependencies for openframeworks
-function installOpenFrameworkDeps()
-{
-	 # openFrameworks
-        pushd ../../openFrameworks
-        	git submodule update --init --recursive
-	popd
-        # openframeworks dependencies from the provided script
-        pushd ../../openFrameworks/scripts/linux/el6
-                ./install_codecs.sh
-                ./install_dependencies.sh
-	popd
-
-}
-
-#Rosser - install / cleanup for opencv
-function installOpenCV()
-{
-	# opencv dependencies
-        yum groupinstall -y "Development Tools"
-        yum install -y gtk+-devel gtk2-devel
-        yum install -y pkgconfig.x86_64
-        yum install -y python
-        yum install -y numpy
-        yum install -y libavc1394-devel.x86_64
-        yum install -y libavc1394.x86_64
-}
-
-function cleanupOpenCV()
-{
-	#opencv
-        yum remove -y gtk+-devel gtk2-devel
-        yum remove -y pkgconfig.x86_64
-	
-}
-
-#Nick - Cleans up openframework dependencies
-function clean_Openframeworks
-{
-	echo "Nothing to delete yet"
-}
-
-#Calum
-function install_libfreenect2()
-{
-	# libfreenect2 dependencies
-        # libusb, requires libudev-devel, libtool from above
-        pushd ../../libfreenect2/depends
-                ./install_libusb.sh
-        popd
-
-        # turbojpeg (libfreenect2)
-        yum install -y turbojpeg-devel
-}
-
-#Calum
-function cleanup_libfreenect2()
-{
-	echo "libfreenect2 cleaned"
+	echo "unistalled tinyosc"
 }
 
 
-if [[ "$1" == "--ogl" ]]; then
-	install_ogl_deps
-        echo "ogl"
-fi
-
-#Nick
-if [[ "$1" == "--ofx" ]]; then
-	installOpenFrameworkDeps
-fi
-
-if [[ "$1" == "--cleanofx" ]]; then
-	clean_Openframeworks
-fi
-
-if [[ "$1" == "--install" ]]; then
-	echo "install"
-
-	yum -y clean all
-	yum -y clean expire-cache
-
-	# add epel and elrepo repos needed form some packages below
-	EL6TYPE=`head -1 /etc/issue | cut -d ' ' -f 1`
-	if [[ "$EL6TYPE" == "Scientific" ]];
-	then
-		yum install -y epel-release elrepo-release
-	else
-		# Presuming CentOS and RHEL
-		# From 'extras'
-		yum install -y epel-release
-		# From elrepo.org
-		rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-		rpm -Uvh http://www.elrepo.org/elrepo-release-6-8.el6.elrepo.noarch.rpm
+for var in "$@"
+do
+	if [ $var == $install_option ]; then
+		mode=$install_option
+	elif [ $var == $cleanup_option ]; then
+		mode=$cleanup_option
+	elif [ $var == $tinyosc_option ]; then
+		tinyosc_option=1
 	fi
 
-	# basic install/compile requirements
-	yum install -y git
-	yum install -y gcc
-	yum install -y make cmake
-	
-	# recent kernel install for the latest USB3 drivers
-	#yum --enablerepo=elrepo-kernel install -y kernel-ml-devel-4.8.7-1.el6.elrepo.x86_64
-	yum --enablerepo=elrepo-kernel install -y kernel-ml kernel-ml-devel
+done
 
-	# OpenGL
-	yum install -y mesa-libGL
-	yum install -y mesa-libGL-devel
-
-	# TODO: refactor somehow; to select dynamically from lspci,
-	#       then download or dpkg-nvidia from elrepo
-	# TODO: this will need to be installed when booted
-	VIDEODRIVERSCRIPT=NVIDIA-Linux-x86_64-375.20.run
-	VIDEODRIVERPATH=XFree86/Linux-x86_64/375.20/$VIDEODRIVERSCRIPT
-	#VIDEODRIVER=XFree86/Linux-x86_64/340.104/NVIDIA-Linux-x86_64-340.104.run
-	wget us.download.nvidia.com/$VIDEODRIVERPATH
-	sh $VIDEODRIVERSCRIPT
-	
-	# install python 34 from epel
-	yum --enablerepo=epel install -y python34.x86_64
-
-	# packages for OpenGL and libusb
-	yum install -y libXmu-devel libXi-devel glut-devel libudev-devel libtool
-
-	# libfreenect2 dependencies
-	# libusb, requires libudev-devel, libtool from above
-#	pushd ../../libfreenect2/depends
-#		./install_libusb.sh
-#	popd
-
-	# turbojpeg (libfreenect2)
-#	yum install -y turbojpeg-devel
-	
-	# opencv dependencies
-#	yum groupinstall -y "Development Tools"
-#	yum install -y gtk+-devel gtk2-devel
-#	yum install -y pkgconfig.x86_64
-#	yum install -y python
-#	yum install -y numpy
-#	yum install -y libavc1394-devel.x86_64
-#	yum install -y libavc1394.x86_64
-	#REFACTOR
-#	installOpenCV
-
-	# libfreenect	
-	# also needs libusb, which is installed above, differently, from libfreenect2
-	# we link to it in build.sh
-	# XXX: OpenNI2 will require cmake3 and gcc 4.8+ from devtoolset-2
-
-        installOpenFrameworkDeps
-
-	yum install -y gstreamer-devel gstreamer-plugins-base-devel
-
-	# alex did tinyosc
-	for var in "$@"
-	do
-		if [ $var == $tinyosc_option ]; then
-			install_tinyosc	
-		elif [ $var == $opencv_option ]; then
-			installOpenCV	
-		elif [ $var == $libfreenect2_option ]; then
-			install_libfreenect2
-		fi
-	done 
-
-# XXX: clean up is outdated; need to be careful
-elif [[ "$1" == "--cleanup" ]]; then
-
-	echo "cleanup"
-	
-	rm -f NVIDIA-Linux-x86_64-375.20.run
-
-	#libusb
-	cd ../../../libfreenect2/depends/libusb_src
-	make distclean
-	cd ..
-	rm -rf libusb
-	rm -rf libusb_src
-
-	#turbojpeg
-	yum remove -y turbojpeg
-	yum remove -y turbojpeg-devel
-
-	#opencv
-#	yum remove -y gtk+-devel gtk2-devel
-#	yum remove -y pkgconfig.x86_64
-	#REFACTOR
-	cleanupOpenCV
-
-	#yum remove -y python
-	yum remove -y numpy
-	yum remove -y libavc1394-devel.x86_64
-	yum remove -y libavc1394.x86_64
-
-	#remove packages installed by yum
-	yum remove -y libXmu-devel libXi-devel glut-devel libudev-devel gstreamer-devel gstreamer-plugins-base-devel
-	for var in "$@"
-	do
-		if [ $var == $tinyosc_option ]; then
-			cleanup_tinyosc
-		elif [ $var == $opencv_option ]; then
-			cleanupOpenCV		
-		elif [ $var == $libfreenect2_option ]; then
-			cleanup_libfreenect2
-		fi
-	done 
+if [ $tinyosc_option == 1 ]; then
+	if [ $mode == $install_option ]; then
+		install_tinyosc
+	elif [ $mode == $cleanup_option ]; then
+		cleanup_tinyosc
+	fi
 fi
+
