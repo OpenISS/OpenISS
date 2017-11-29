708dc3da13c57e6ab9ee3fe77e4b65bf34cb6019
Calum Phillips <calum.phillips.code@gmail.com>
2017-11-29 05:42:01 -0500

    Not working state, but progress. Modifed the driver to use a proper function that it actually as access to. Fixed the image export function before somehow breaking the image export function. Fixed any minor errors in the reading from a file. Fixed errors in the drivers reading and writing functions.

--- src/api/c/quiz3/main.c
+++ src/api/c/quiz3/main.c
@@ -6,40 +6,42 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <math.h>
 
 #ifndef HEXDUMP_COLS
 #define HEXDUMP_COLS 8
 #endif
 
 #define DEVICE_NAME "/dev/ISSIMAGEDRV"
-#define FILE_NAME_1 "/home/rosser/atsm/examples/c/syscalls/file/ifv/Makefile"
-#define FILE_NAME_2 "/home/rosser/atsm/examples/c/syscalls/file/ifv/Makefile.orig"
+#define FILE_NAME_1 "/boot/initramfs-4.14.0-1.el6.elrepo.x86_64.img"
+#define FILE_NAME_2 "/bin/vi"
 
 void hex_to_image(char outputf[], int hexSize, char hexdump[])
 {
 	int fd;
-	char num [13];
+	int numSize = 17;
+	char num [numSize];
 	int iter = 0;
 
 	/*Create the file with the given ouput filename*/
 	fd = open(outputf, O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH);
 
 	/* Assign the width and the height of the image */
-	int height, width;
-	width = hexSize / 2;
+	int height = 0, width = 0;
+	width = (int)(hexSize / 3.0);
 	
 	if(hexSize % 2 == 0)
-		width = hexSize / 2;
+		height = hexSize / 2;
 	else
-		width = (hexSize / 2) -1;	
+		height = (hexSize / 2) -1;	
+	height = 1;
 	
 	/* Input the header and image size to the file */
-	snprintf(num, 10, "P3\n%i %i\n", width, height);
+	snprintf(num, numSize, "P6\n%i %i\n125\n", width, height);
 	write(fd, num, strlen(num));
 
 	int i;
-	for (i = 0; i < hexSize; i++)
+	for (i = 0; i < width * height; i++)
 	{
 		char toWrite[1] = {hexdump[i]};
 		write(fd, toWrite, 1);
@@ -47,15 +49,23 @@ void hex_to_image(char outputf[], int hexSize, char hexdump[])
 		/* if R, G, and B have been outputted, go to a new line */
 		if(iter == 2)
 		{	
-			write(fd, (char*) '\n', 1);
+			/*write(fd, (char*) '\n', 1);*/
 			iter = 0;
 		}		
 		else
 		{
-			write(fd, (char*) ' ', 1);
+			/*write(fd, (char*) ' ', 1);*/
 			iter++;
 		}
 			
+	}
+
+	while (iter != 0)
+	{
+		/*write(fd, "\0 ", 2);*/
+		iter++;
+		if (iter > 2)
+			break;
 	}
 	
 	close(fd);
@@ -65,22 +75,17 @@ void hex_to_image(char outputf[], int hexSize, char hexdump[])
 
 int main(int argc, char* argv[])
 {
-
-	int fd = 0, deviceFd = 0, readSize = 100, bytesRead, forkId;
+	int fd = 0, deviceFd = 0, readSize = 99, bytesRead, forkId = 1;
 	//the file name
 	char buf[readSize];
 	char* fileName;
 
-//	hexDump("strtest", fp, lSize);
-
-
-    //printf("%i", lSize);
-
-	forkId = fork();
+	/*forkId = fork();*/
 
 	if(forkId == 0)
 	{
 		fileName = FILE_NAME_1;
+		return 0;
 	}
 	else
 	{
@@ -93,10 +98,21 @@ int main(int argc, char* argv[])
 		{
 			bytesRead = read(fd, buf, readSize);
 			write(deviceFd, buf, bytesRead);
-			printf("%i\n", bytesRead);
 
 			close(fd);
 		}
+		else
+		{
+			perror("File couldn't be opened!!!!");
+			return 1;
+		}
+
+		int bufferSize = 99;
+		char readBuffer[bufferSize];
+		int readSize;
+
+		readSize = read(deviceFd, readBuffer, bufferSize);
+		hex_to_image(forkId == 0 ? "child_image.ppd" : "parent_image.ppd", readSize, readBuffer);
 
 		close(deviceFd);
 	}
