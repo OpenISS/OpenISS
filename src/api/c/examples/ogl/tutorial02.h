aa4706cc01f09c43d3a71ab7e9901d3a8d5b1f0a
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2017-12-12 22:06:32 -0500

    added ogl example for tutorial02 with c wrapping, makefile, and shader files

--- src/api/c/examples/ogl/tutorial02.h
+++ src/api/c/examples/ogl/tutorial02.h
@@ -1,14 +1,8 @@
 #ifndef _TUTORIAL02_H
 #define _TUTORIAL02_H
 
-typedef struct
-{
-    void (*02_init) (void);
-    void (*02_draw) (void);
-    void (*02_free) (void);
-}
-t_iss_02_ops;
-
-extern t_iss_02_ops p_o02EmptyOpenGLTest;
+int two_init();
+void two_draw();
+void two_free();
 
 #endif
---
+++ src/api/c/examples/ogl/tutorial02Wrapper.h
@@ -0,0 +1,21 @@
+#include <GL/glew.h>
+
+#ifdef __cplusplus
+#define EXTERNC extern "C"
+#else
+#define EXTERNC
+#endif
+
+#ifdef __cplusplus
+
+extern "C" {
+#else
+
+#endif
+
+/*extern "C" */
+GLuint CLoadShaders();
+
+#ifdef __cplusplus
+}
+#endif
