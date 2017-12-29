353e7e104065ac576700c6d8ef069783120c1297
Serguei Mokhov <mokhov@cse.concordia.ca>
2017-12-28 22:51:31 -0500

    [vfx] refactor a bit init code for testing in vfx.c example

--- src/api/c/vfx.c
+++ src/api/c/vfx.c
@@ -24,14 +24,6 @@ void vfx_free(void);
 
 void vfx_init()
 {
-}
-
-void vfx_free()
-{
-}
-
-void vfx_draw()
-{
     /* Initialise GLFW */
     if( !glfwInit() )
     {
@@ -47,7 +39,7 @@ void vfx_draw()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
     /* Open a window and create its OpenGL context */
-    window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
+    window = glfwCreateWindow( 1024, 768, "OpenISS GL3 VFX 101", NULL, NULL);
     if( window == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
         getchar();
@@ -67,7 +59,14 @@ void vfx_draw()
     
     /* Ensure we can capture the escape key being pressed below */
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-    
+}
+
+void vfx_free()
+{
+}
+
+void vfx_draw()
+{
     /* Dark blue background */
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
     
