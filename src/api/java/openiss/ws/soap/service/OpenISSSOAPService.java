66f2a68d83596969a2f94b229a660e1ab955fefd
Konstantinos Psimoulis <kosta@noima.com>
2018-02-21 22:30:21 -0500

    changing SOAP service to RPC in order to get back result

--- src/api/java/openiss/ws/soap/service/OpenISSSOAPService.java
+++ src/api/java/openiss/ws/soap/service/OpenISSSOAPService.java
@@ -1,10 +1,16 @@
 package api.java.openiss.ws.soap.service;
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.ws.BindingType;
 
 @WebService
+@BindingType(value = "http://java.sun.com/xml/ns/jaxws/2003/05/soap/bindings/HTTP/")
+@SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface OpenISSSOAPService {
 
     @WebMethod
-    byte[] getFrame(String type);
+    byte[] getFrame(@WebParam(name = "type") String type);
+
 }
