cbba092a92ca9a3d3c752ba23b731782441e15c8
Konstantinos Psimoulis <kosta@noima.com>
2018-02-23 21:53:13 -0500

    Yasmine: Added function to display image from data

--- src/api/java/openiss/ws/soap/endpoint/Test.java
+++ src/api/java/openiss/ws/soap/endpoint/Test.java
@@ -20,7 +20,8 @@ import java.io.InputStream;
 //import org.openkinect.Acceleration;
 //import org.openkinect.Image;
 //import org.openkinect.LEDStatus;
-public class Test {
+
+public class Test{
     //  static Context ctx;
 //  static Device dev;
     public static void main(String[] args) {
@@ -60,12 +61,14 @@ public class Test {
                             try {
 //                                String v = new String(frame.array(), "UTF-8");
 //                                System.out.println(v);
-                                File file = new File("filename");
-                                FileChannel wChannel = new FileOutputStream(file, false).getChannel();
-                                wChannel.write(frame);
-                                wChannel.close();
+                                byte[] temp = new byte[frame.remaining()];
+                                frame.get(temp);
+//                                File file = new File("filename");
+//                                FileChannel wChannel = new FileOutputStream(file, false).getChannel();
+//                                wChannel.write(ppm(640, 480, 5, temp));
+//                                wChannel.close();
 //                                device.color(null);
-//                                ImageIO.write(frame, "jpg", new File("kinect.color.jpg"));
+                                ImageIO.write(ppm(640, 480, 255, temp), "jpg", new File("kinect.color.jpg"));
 //                                byte[] b = new byte[frame.remaining()];
 //                                frame.get(b, 0, b.length);
 //
@@ -92,6 +95,52 @@ public class Test {
             });
         } else {
             System.err.println("WARNING: No kinects detected, hardware tests will be implicitly passed.");
+        }
+    }
+
+    static public BufferedImage ppm(int width, int height, int maxcolval, byte[] data){
+        if(maxcolval<256){
+            BufferedImage image=new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+            int r,g,b,k=0,pixel;
+            if(maxcolval==255){                                      // don't scale
+                for(int y=0;y<height;y++){
+                    for(int x=0;(x<width)&&((k+3)<data.length);x++){
+                        r=data[k++] & 0xFF;
+                        g=data[k++] & 0xFF;
+                        b=data[k++] & 0xFF;
+                        pixel=0xFF000000+(r<<16)+(g<<8)+b;
+                        image.setRGB(x,y,pixel);
+                    }
+                }
+            }
+            else{
+                for(int y=0;y<height;y++){
+                    for(int x=0;(x<width)&&((k+3)<data.length);x++){
+                        r=data[k++] & 0xFF;r=((r*255)+(maxcolval>>1))/maxcolval;  // scale to 0..255 range
+                        g=data[k++] & 0xFF;g=((g*255)+(maxcolval>>1))/maxcolval;
+                        b=data[k++] & 0xFF;b=((b*255)+(maxcolval>>1))/maxcolval;
+                        pixel=0xFF000000+(r<<16)+(g<<8)+b;
+                        image.setRGB(x,y,pixel);
+                    }
+                }
+            }
+            return image;
+        }
+        else{
+
+
+            BufferedImage image=new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+            int r,g,b,k=0,pixel;
+            for(int y=0;y<height;y++){
+                for(int x=0;(x<width)&&((k+6)<data.length);x++){
+                    r=(data[k++] & 0xFF)|((data[k++] & 0xFF)<<8);r=((r*255)+(maxcolval>>1))/maxcolval;  // scale to 0..255 range
+                    g=(data[k++] & 0xFF)|((data[k++] & 0xFF)<<8);g=((g*255)+(maxcolval>>1))/maxcolval;
+                    b=(data[k++] & 0xFF)|((data[k++] & 0xFF)<<8);b=((b*255)+(maxcolval>>1))/maxcolval;
+                    pixel=0xFF000000+(r<<16)+(g<<8)+b;
+                    image.setRGB(x,y,pixel);
+                }
+            }
+            return image;
         }
     }
 }
