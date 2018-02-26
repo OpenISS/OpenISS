ca2eb951291432c3f2a0f4bba3ca910de4b9840c
Konstantinos Psimoulis <kosta@noima.com>
2018-02-25 22:13:07 -0500

    More refactoring

--- src/api/java/openiss/ws/soap/endpoint/OpenISSSOAPServicePublisher.java
+++ src/api/java/openiss/ws/soap/endpoint/OpenISSSOAPServicePublisher.java
@@ -1,26 +1,13 @@
 package api.java.openiss.ws.soap.endpoint;
 
 import api.java.openiss.ws.soap.service.OpenISSSOAPServiceImpl;
+import javax.xml.ws.Endpoint;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.xml.ws.Endpoint;  
+public class OpenISSSOAPServicePublisher {
 
-/**
- * Endpoint publisher
- *
- * @author zephi
- */
 
-@WebServlet(name = "OpenISSSOAPServicePublisher")
-public class OpenISSSOAPServicePublisher extends HttpServlet{
-    public void init(ServletConfig config) throws ServletException {
+    public static void main(String[] args) {
         Endpoint.publish("http://localhost:7779/ws/xmlread", new OpenISSSOAPServiceImpl());
-   }
-//    public static void main(String[] args) {
-
-//       Endpoint.publish("http://localhost:7779/ws/xmlread", new OpenISSSOAPServiceImpl());
-//    }
+        System.out.println("SOAP Service listening on http://localhost:7779/ws/xmlread?wsdl");
+    }
 }  
