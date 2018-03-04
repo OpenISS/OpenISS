d8235bb990a66647424b5020280d60716f4f7121
Palmirouze <Palmirouze@gmail.com>
2018-03-04 17:36:54 -0500

    Changed filesystem looping and implementation

--- src/api/java/openiss/ws/soap/service/OpenISSSOAPServiceImpl.java
+++ src/api/java/openiss/ws/soap/service/OpenISSSOAPServiceImpl.java
@@ -1,5 +1,6 @@
 package openiss.ws.soap.service;
 
+import com.sun.javafx.runtime.SystemProperties;
 import openiss.Kinect;
 import openiss.ws.soap.endpoint.ServicePublisher;
 
@@ -9,79 +10,49 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static openiss.ws.soap.endpoint.ServicePublisher.kinect;
 
 @WebService(endpointInterface="openiss.ws.soap.service.OpenISSSOAPService")
 public class OpenISSSOAPServiceImpl implements OpenISSSOAPService{
 
-
     private static String colorFileName = "src/api/java/openiss/ws/soap/service/image_example.jpg";
     private static String depthFileName = "src/api/java/openiss/ws/soap/service/image_example.jpg";
     static String FAKENECT_PATH = System.getenv("FAKENECT_PATH");
 
-    public String getFileName(String type) {
-
-        if(type.equalsIgnoreCase("color")){
-            return colorFileName;
-        }
-        else{
-            return depthFileName;
-
-        }
-
-    }
-
-    public void setColorFileName(String fileName) {
-
-        this.colorFileName = fileName;
-
-    }
-
-    public void setDepthFileName(String fileName) {
-
-        this.depthFileName = fileName;
-
-    }
-
-
-
     public byte[] getFrame(String type) {
 
-        byte[] ppmImageInByte = new byte[0];
-        byte[] pgmImageInByte = new byte[0];
+        byte[] imageInBytes = new byte[0];
         byte[] jpgImageInByte = new byte[0];
 
-        BufferedImage originalImage = null;
-
         try {
-
-            String colorSrc = FAKENECT_PATH + "/" + getFileName("color");
-            String depthSrc = FAKENECT_PATH + "/" + getFileName("depth");
-
             BufferedImage image;
-            // convert BufferedImage to byte array
+
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-            //Kinect kinect = new Kinect();
-
+            // validity checks
             if (!type.equals("color") && !type.equals("depth")) {
                 throw new IllegalArgumentException("Bad type for getFrame: " + type);
             }
 
+            // file system routine
             if (ServicePublisher.USE_FILESYSTEM) {
-                File colorInitialFile = new File(colorSrc);
-                File depthInitialFile = new File(depthSrc);
-                ppmImageInByte = Files.readAllBytes(colorInitialFile.toPath());
-                pgmImageInByte = Files.readAllBytes(depthInitialFile.toPath());
+                String colorSrc = FAKENECT_PATH + "/" + getFileName("color");
+                String depthSrc = FAKENECT_PATH + "/" + getFileName("depth");
+                String source;
 
-                if(type.equals("color")){
-                    image = Kinect.processPPMImage(640, 480, ppmImageInByte);
-
-                }else{
-                    image = Kinect.processPPMImage(640, 480, pgmImageInByte);
-
+                // set source for file
+                if(type.equals("color")) {
+                    source = colorSrc;
+                } else {
+                    source = depthSrc;
                 }
+
+                // read bytes at the current path
+                File frame = new File(source);
+                imageInBytes = Files.readAllBytes(frame.toPath());
+                image = Kinect.processPPMImage(640, 480, imageInBytes);
             }
             else {
                 if (type.equals("color")) {
@@ -99,6 +70,7 @@ public class OpenISSSOAPServiceImpl implements OpenISSSOAPService{
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return jpgImageInByte;
     }
 
@@ -122,8 +94,7 @@ public class OpenISSSOAPServiceImpl implements OpenISSSOAPService{
         double weight = 0.5;
 
         // init images
-        byte[] ppmImageInByte;
-        byte[] pgmImageInByte = new byte[0];
+        byte[] imageInBytes;
 
         BufferedImage image_1 = null;
         BufferedImage image_2 = null;
@@ -142,27 +113,24 @@ public class OpenISSSOAPServiceImpl implements OpenISSSOAPService{
 
 
             if (ServicePublisher.USE_FILESYSTEM) {
-
                 String colorSrc = FAKENECT_PATH + "/" + getFileName("color");
                 String depthSrc = FAKENECT_PATH + "/" + getFileName("depth");
-
-                File colorInitialFile = new File(colorSrc);
-                File depthInitialFile = new File(depthSrc);
-                ppmImageInByte = Files.readAllBytes(colorInitialFile.toPath());
-                pgmImageInByte = Files.readAllBytes(depthInitialFile.toPath());
-
-                if(type.equals("color")){
-                    image_2 = Kinect.processPPMImage(640, 480, ppmImageInByte);
-
-                }else{
-                    image_2 = Kinect.processPPMImage(640, 480, pgmImageInByte);
-
+                String source;
+                if(type.equals("color")) {
+                    source = colorSrc;
+                } else {
+                    source = depthSrc;
                 }
 
-            } else {
+                File frame = new File(source);
+                imageInBytes = Files.readAllBytes(frame.toPath());
+                image_2 = Kinect.processPPMImage(640, 480, imageInBytes);
+            }
+            else {
                 if (type.equals("color")) {
                     image_2 = kinect.getVideoImage();
-                } else {
+                }
+                else {
                     image_2 = kinect.getDepthImage();
                 }
             }
@@ -210,39 +178,20 @@ public class OpenISSSOAPServiceImpl implements OpenISSSOAPService{
         return imageInByte;
     }
 
-
-    // helper for getting bytes of an image
-    public byte[] getBytes(String imageName) {
-        byte[] imageInByte = new byte[0];
-
-        BufferedImage originalImage = null;
-        try {
-            originalImage = ImageIO.read(new File(
-                    imageName));
-            // convert BufferedImage to byte array
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(originalImage, "jpg", baos);
-            baos.flush();
-            imageInByte = baos.toByteArray();
-            baos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public String getFileName(String type) {
+        if(type.equalsIgnoreCase("color")){
+            return colorFileName;
+        } else {
+            return depthFileName;
         }
-        return imageInByte;
     }
 
-    // creates a jpeg from a byte array
-    // technically no use for that since we unmarshall client side in Node.js
-    public void fromByteToJpg(byte[] imageBytes) {
-        InputStream in = new ByteArrayInputStream(imageBytes);
+    public void setColorFileName(String fileName) {
+        colorFileName = fileName;
+    }
 
-        try {
-            BufferedImage bImageFromConvert = ImageIO.read(in);
-            ImageIO.write(bImageFromConvert, "jpg", new File(
-                    "new_image_example.jpg"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void setDepthFileName(String fileName) {
+        depthFileName = fileName;
     }
 
 }
