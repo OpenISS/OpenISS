427d7177ca4751e3d9576c4c235d4ce65285830e
Palmirouze <Palmirouze@gmail.com>
2018-03-11 15:01:24 -0400

    fixed depth view (added PGM function)

--- src/api/java/openiss/Kinect.java
+++ src/api/java/openiss/Kinect.java
@@ -46,7 +46,7 @@ import org.openkinect.freenect.VideoHandler;
 public class Kinect {
 
 	static byte[] color;
-	static byte[] depth;
+	static ShortBuffer depth;
 
 	Method depthEventMethod;
 	Method videoEventMethod;
@@ -198,11 +198,7 @@ public class Kinect {
 			final Kinect ref = this;
 			device.startDepth(new DepthHandler() {
 				public void onFrameReceived(FrameMode mode, ByteBuffer frame, int timestamp) {
-
-					depth = new byte[frame.remaining()];
-					frame.get(depth);
-					frame.flip();
-
+					depth = frame.asShortBuffer();
 				}
 			});
 		}
@@ -315,7 +311,7 @@ public class Kinect {
 	 */	
 	public BufferedImage getDepthImage() {
 
-		return processPPMImage(640, 480, depth);
+		return processPGMImage(640, 480, depth);
 
 	}
 	
@@ -353,6 +349,93 @@ public class Kinect {
 		}
 		return image;
 	}
-	
+
+
+
+	static public BufferedImage processPGMImage(int width, int height, ShortBuffer data) {
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		int red, green, blue, pixel;
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+
+				int offset = x + y*width;
+				short depth = data.get(offset);
+				image.setRGB(x, y, depth2rgb(depth));
+			}
+		}
+
+		return image;
+	}
+
+	static int depth2rgb(short depth) {
+		int r,g,b;
+
+		float v = depth / 2047f;
+		v = (float) Math.pow(v, 3)* 6;
+		v = v*6*256;
+
+		int pval = Math.round(v);
+		int lb = pval & 0xff;
+		switch (pval>>8) {
+			case 0:
+				b = 255;
+				g = 255-lb;
+				r = 255-lb;
+				break;
+			case 1:
+				b = 255;
+				g = lb;
+				r = 0;
+				break;
+			case 2:
+				b = 255-lb;
+				g = 255;
+				r = 0;
+				break;
+			case 3:
+				b = 0;
+				g = 255;
+				r = lb;
+				break;
+			case 4:
+				b = 0;
+				g = 255-lb;
+				r = 255;
+				break;
+			case 5:
+				b = 0;
+				g = 0;
+				r = 255-lb;
+				break;
+			default:
+				r = 0;
+				g = 0;
+				b = 0;
+				break;
+		}
+
+		int pixel = (0xFF) << 24
+				| (b & 0xFF) << 16
+				| (g & 0xFF) << 8
+				| (r & 0xFF) << 0;
+
+		return pixel;
+	}
+
+	static int depth2intensity(short depth) {
+		int d = Math.round((1 - (depth / 2047f)) * 255f);
+		int pixel = (0xFF) << 24
+				| (d & 0xFF) << 16
+				| (d & 0xFF) << 8
+				| (d & 0xFF) << 0;
+
+		return pixel;
+	}
+
+
+
+
+
+
 
 }
