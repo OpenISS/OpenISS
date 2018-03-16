2ad403d10b743bae8a7dadec4c7fa207b8165f85
Konstantinos Psimoulis <kosta@noima.com>
2018-03-16 19:28:28 -0400

    Adding maven and jersey support for Rest services

---
+++ src/api/java/openiss/ws/rest/src/main/java/ca/concordia/OpenISSRestService.java
@@ -0,0 +1,22 @@
+package ca.concordia;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/openiss")
+public class OpenISSRestService {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+}
---
+++ src/api/java/openiss/ws/rest/web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kosta
+  Date: 2018-03-16
+  Time: 6:45 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <p><a href="rest/myresource">Jersey resource</a>
+  $END$
+  </body>
+</html>
