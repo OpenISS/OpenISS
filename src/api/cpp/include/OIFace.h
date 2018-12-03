fd4a5d153d14d841fcb323726d078d7394435355
Kellers176 <Kelly.Herstine@mymail.champlain.edu>
2018-12-03 14:27:14 -0500

    Changed two files for part three of Assignment 3.

---
+++ src/api/cpp/include/OIFace.h
@@ -0,0 +1,21 @@
+//
+//  OIFace.hpp
+//  Face
+//
+//  Created by Jashanjot Singh on 2018-07-22.
+
+#ifndef OIFace_h
+#define OIFace_h
+
+#include <stdio.h>
+
+struct OpenISS_OIFace
+{
+
+struct OIFace
+{
+};
+
+};
+
+#endif /* OIFace_hpp */
