d83f40f5167b4d6de90c0a6fb45c37cf4461a0fa
Konstantinos Psimoulis <kosta@noima.com>
2021-05-10 16:36:54 -0400

    Moved hardcoded paths to config #54

--- src/api/java/openiss/utils/OpenISSConfig.java
+++ src/api/java/openiss/utils/OpenISSConfig.java
@@ -5,4 +5,5 @@ public class OpenISSConfig {
     public static boolean USE_FAKENECT = false; // Requires FAKENECT_PATH with recorded session
     public static boolean USE_STATIC_IMAGES = true; // Uses two static images - color_example.jpg and depth_example.jpg
     public static boolean USE_OPENCV = true; // You should disable this if you cannot install OPENCV
+    public static final String OUTPUT_FILE_DIR = "/Users/kosta/Desktop/";
 }
